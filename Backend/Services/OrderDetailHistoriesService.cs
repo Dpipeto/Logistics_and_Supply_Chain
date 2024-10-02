33e03b5c8ac9d4b62315cb6cb61b3ebb4352e17c
Diego Romero <jd.romerojmz23@gmail.com>
2024-10-01 22:41:06 -0500

    Update OrderDetalilHistories class

---
+++ Backend/Services/OrderDetailHistoriesService.cs
@@ -0,0 +1,43 @@
+﻿using Backend.Model;
+using Backend.Repositories;
+
+namespace Backend.Services
+{
+    public interface IOrderDetailHistoriesService
+    {
+        Task<IEnumerable<OrderDetailHistories>> GetAllOrderDetailHistoriesAsync();
+        Task<OrderDetailHistories?> GetOrderDetailHistoriesByIdAsync(int id);
+        Task CreateOrderDetailHistoriesAsync(OrderDetailHistories orderDetailHistories);
+        Task UpdateOrderDetailHistoriesAsync(OrderDetailHistories orderDetailHistories);
+        Task SoftDeleteOrderDetailHistoriesAsync(int id);
+    }
+    public class OrderDetailHistoriesService : IOrderDetailHistoriesService
+    {
+        private readonly IOrderDetailHistoriesRepository _OrderDetailHistoriesRepository;
+
+        public OrderDetailHistoriesService(IOrderDetailHistoriesRepository OrderDetailHistoriesRepository)
+        {
+            _OrderDetailHistoriesRepository = OrderDetailHistoriesRepository;
+        }
+        public async Task<IEnumerable<OrderDetailHistories>> GetAllOrderDetailHistoriesAsync()
+        {
+            return await _OrderDetailHistoriesRepository.GetAllOrderDetailHistoriesAsync();
+        }
+        public async Task<OrderDetailHistories?> GetOrderDetailHistoriesByIdAsync(int id)
+        {
+            return await _OrderDetailHistoriesRepository.GetOrderDetailHistoriesByIdAsync(id);
+        }
+        public async Task CreateOrderDetailHistoriesAsync(OrderDetailHistories orderDetailHistories)
+        {
+            await _OrderDetailHistoriesRepository.CreateOrderDetailHistoriesAsync(orderDetailHistories);
+        }
+        public async Task UpdateOrderDetailHistoriesAsync(OrderDetailHistories orderDetailHistories)
+        {
+            await _OrderDetailHistoriesRepository.UpdateOrderDetailHistoriesAsync(orderDetailHistories);
+        }
+        public async Task SoftDeleteOrderDetailHistoriesAsync(int id)
+        {
+            await _OrderDetailHistoriesRepository.SoftDeleteOrderDetailHistoriesAsync(id);
+        }
+    }
+}
