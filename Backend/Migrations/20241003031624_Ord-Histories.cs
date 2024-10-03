b681fca7f8ab764d0f16dacd480ef4163ce04e22
DAVID FELIPE GARZÓN BARBOSA <dpipegarzon@gmail.com>
2024-10-02 23:22:11 -0500

    update triggers

---
+++ Backend/Migrations/20241003031624_Ord-Histories.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrdHistories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ordersHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    TypeOrder = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Packagesender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PackageReceive = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrderValue = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SenderAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SenderPhone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SenderEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ModifiedDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    User = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrderDetail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrderStatusType = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ordersHistories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ordersTrackingHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ModifiedDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Order = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Dealer = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ordersTrackingHistories", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ordersHistories");
+
+            migrationBuilder.DropTable(
+                name: "ordersTrackingHistories");
+        }
+    }
+}
